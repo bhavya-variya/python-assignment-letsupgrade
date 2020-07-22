24cc682bae211b5fd1d70e59d40f291121317d9f
bhavya-variya <68175291+bhavya-variya@users.noreply.github.com>
2020-07-22 12:08:47 +0530

    Add files via upload

---
+++ day2assignment.py
@@ -0,0 +1,56 @@
+print ('hello python')
+print ('hello \
+python')
+print ('hello\n python')
+print ('hello\tpython')
+print ("hello's python")
+print ('hello python\a')
+
+print (5+2)
+print (5-2)
+print (5*2)
+print (5/2)
+print (5**2)
+print (5//2)
+print (5**-2)
+print (5%2)
+
+name = "bhavya"
+marks = 90
+age = 17
+print ("my name is ",name,"my marks is ",marks,"my age is ",age )
+# print ("my name is %f my marks is %d my age is %s"%(name ,marks ,age))
+
+
+#same is only valid between -5 to  256
+a= 10
+b=10
+a=b
+print (id(a))
+print (id(b))
+print (bin(a))
+print (bin(b))
+
+c=-6
+d=-6
+print (id(c))
+print (id(d))
+
+
+
+# true and false = false      1 and 0 = 0
+# false and true = false      0 and 1 = 0
+# true and true = true        1 and 1 = 1
+# false and false = false     0 and 0 = 0
+
+# true or false = true        1 or 0 = 1
+# false or true = true        0 or 1 = 1
+# true or true = true         1 or 1 = 1
+# false or false = false      0 or 0 = 0
+
+x= 1
+y=20
+print (x>y)
+print (x<y)
+# print (x<y and 10>m)
+print (x>y and 10>m)
