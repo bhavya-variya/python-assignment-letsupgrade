21313189a71f1d864a8e92529345f1d346db2595
bhavya-variya <68175291+bhavya-variya@users.noreply.github.com>
2020-07-23 19:54:05 +0530

    Add files via upload

---
+++ day4assignment2.py
@@ -0,0 +1,16 @@
+str1 = "hello there"
+str2 = "HELLO THERE"
+str3 = "Hello there"
+str4 = "HELLO THERe"
+print (str1)
+print (str2)
+print (str1.islower(),str1)
+print (str2.islower(),str2)
+print (str3.islower(),str3)
+print (str4.islower(),str4)
+
+
+print (str1.isupper(),str1)
+print (str2.isupper(),str2)
+print (str3.isupper(),str3)
+print (str4.isupper(),str4)
